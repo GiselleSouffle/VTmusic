f396df9a88bc94299c7d25d972e58b49032d0aeb
GiselleSouffle <a22311123@uthermosillo.edu.mx>
2024-03-20 11:39:07 -0700

    Se elimino un campo

--- api/VTmusic/database/migrations/2024_01_19_202358_update_artists_table.php
+++ api/VTmusic/database/migrations/2024_01_19_202358_update_artists_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::rename('album', 'artists' );
     }
 
     /**
@@ -19,6 +18,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('artists');
     }
 };
