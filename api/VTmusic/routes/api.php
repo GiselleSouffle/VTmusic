0384018c44b7135c39600dc8d815ede476a73b27
GiselleSouffle <a22311123@uthermosillo.edu.mx>
2024-03-20 13:23:23 -0700

    ahora si2

--- api/VTmusic/routes/api.php
+++ api/VTmusic/routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\generController;
+use App\Http\Controllers\Api\GenerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\artistController;
-use App\Http\Controllers\Api\albumController;
-use App\Http\Controllers\Api\commentController;
-use App\Http\Controllers\Api\songController;
-use App\Http\Controllers\Api\userController;
-use App\Http\Controllers\Api\voteController;
+use App\Http\Controllers\Api\ArtistController;
+use App\Http\Controllers\Api\AlbumController;
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\SongController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VoteController;
 
 
 
@@ -28,39 +28,39 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/albums',[albumController::class, 'list']);
-Route::get('/albums/{id}',[albumController::class, 'show']);
-Route::post('/albums/create',[albumController::class, 'create']);
-Route::post('/albums/{id}/update',[albumController::class, 'update']);
+Route::get('/albums',[AlbumController::class, 'list']);
+Route::get('/albums/{id}',[AlbumController::class, 'show']);
+Route::post('/albums/create',[AlbumController::class, 'create']);
+Route::post('/albums/{id}/update',[AlbumController::class, 'update']);
 
-Route::get('/artists',[artistController::class, 'list']);
-Route::get('/artists/{id}',[artistController::class, 'show']);
-Route::post('/artists/create',[artistController::class, 'create']);
-Route::post('/artists/{id}/update',[artistController::class, 'update']);
+Route::get('/artists',[ArtistController::class, 'list']);
+Route::get('/artists/{id}',[ArtistController::class, 'show']);
+Route::post('/artists/create',[ArtistController::class, 'create']);
+Route::post('/artists/{id}/update',[ArtistController::class, 'update']);
 
-Route::get('/comments',[commentController::class, 'list']);
-Route::get('/comments/{id}',[commentController::class, 'show']);
-Route::post('/comments/create',[commentController::class, 'create']);
-Route::post('/comments/{id}/update',[commentController::class, 'update']);
+Route::get('/comments',[CommentController::class, 'list']);
+Route::get('/comments/{id}',[CommentController::class, 'show']);
+Route::post('/comments/create',[CommentController::class, 'create']);
+Route::post('/comments/{id}/update',[CommentController::class, 'update']);
 
-Route::get('/geners',[generController::class, 'list']);
-Route::get('/geners/{id}',[generController::class, 'show']);
-Route::post('/geners/create',[generontroller::class, 'create']);
-Route::post('/geners/{id}/update',[generController::class, 'update']);
+Route::get('/geners',[GenerController::class, 'list']);
+Route::get('/geners/{id}',[GenerController::class, 'show']);
+Route::post('/geners/create',[GenerController::class, 'create']);
+Route::post('/geners/{id}/update',[GenerController::class, 'update']);
 
-Route::get('/songs',[songController::class, 'list']);
-Route::get('/songs/{id}',[songController::class, 'show']);
-Route::post('/songs/create',[songController::class, 'create']);
-Route::post('/songs/{id}/update',[songController::class, 'update']);
+Route::get('/songs',[SongController::class, 'list']);
+Route::get('/songs/{id}',[SongController::class, 'show']);
+Route::post('/songs/create',[SongController::class, 'create']);
+Route::post('/songs/{id}/update',[SongController::class, 'update']);
 
-Route::get('/users',[userController::class, 'list']);
-Route::get('/users/{id}',[userController::class, 'show']);
-Route::post('/users/create',[userController::class, 'create']);
-Route::post('/users/{id}/update',[userController::class, 'update']);
+Route::get('/users',[UserController::class, 'list']);
+Route::get('/users/{id}',[UserController::class, 'show']);
+Route::post('/users/create',[UserController::class, 'create']);
+Route::post('/users/{id}/update',[UserController::class, 'update']);
 
-Route::get('/votes',[voteController::class, 'list']);
-Route::get('/votes/{id}',[voteController::class, 'show']);
-Route::post('/votes/create',[voteController::class, 'create']);
-Route::post('/votes/{id}/update',[voteController::class, 'update']);
+Route::get('/votes',[VoteController::class, 'list']);
+Route::get('/votes/{id}',[VoteController::class, 'show']);
+Route::post('/votes/create',[VoteController::class, 'create']);
+Route::post('/votes/{id}/update',[VoteController::class, 'update']);
 
 Route::post('/login',[AuthController::class, 'login']);
