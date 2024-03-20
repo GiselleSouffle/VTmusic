2a35777f36dc483f4998924f8b59f992384fbcb6
GiselleSouffle <a22311123@uthermosillo.edu.mx>
2024-03-20 13:49:13 -0700

    ahora si4

--- api/VTmusic/database/migrations/2024_01_19_203358_update_artists_table.php
+++ api/VTmusic/database/migrations/2024_01_19_203358_update_artists_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('artists', function (Blueprint $table) {
-            $table->id();
             $table->timestamps();
         });
     }
